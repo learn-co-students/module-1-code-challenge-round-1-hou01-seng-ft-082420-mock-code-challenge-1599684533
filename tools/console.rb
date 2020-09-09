8de59e7ffc0f05b6735c4c4caf68bcf7bd4e830c
Far.k <farzana.mkarim@gmail.com>
2020-09-09 17:39:15 -0500

    submission for deadline

--- tools/console.rb
+++ tools/console.rb
@@ -10,7 +10,17 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+s1 = Student.new("Nathan")
+s2 = Student.new("Lucas")
+s3 = Student.new("James")
 
+t1 = Tutor.new("Miss L")
+t2 = Tutor.new("Mr S")
+t3 = Tutor.new("Miss T")
+
+c1 = Course.new(t1, s2, "Medicine")
+c2 = Course.new(t2, s1, "Psychology")
+c3 = Course.new(t3, s3, "Engineering")
 
 
 
